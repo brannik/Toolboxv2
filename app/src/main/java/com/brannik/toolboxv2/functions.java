46aafe75bd4291083f0272b90cd84f80bf8d8249
brannik <georgi.vtu.mi@gmail.com>
2020-11-01 01:07:08 +0200

    control over WIFI

--- app/src/main/java/com/brannik/toolboxv2/functions.java
+++ app/src/main/java/com/brannik/toolboxv2/functions.java
@@ -8,11 +8,24 @@ public class functions {
     Context applicationContext = MainActivity.getContextOfApplication();
     SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(applicationContext);
     String drlState,interState,ampState,dvrState,defDrlState,defInterState,defAmpState,defDvrState,drlDelay,interDelay,ampDelay,dvrDelay,daytimeStart,daytimeEnd;
-    
+
     public void getSettings(){
         // read shared prefs
         if (prefs.contains("drlState") & prefs.contains("daytimeEnd")) {
             drlState = prefs.getString("drlState",null);
+            interState = prefs.getString("interState",null);
+            ampState = prefs.getString("ampState",null);
+            dvrState = prefs.getString("dvrState",null);
+            defDrlState = prefs.getString("defDrlState",null);
+            defInterState = prefs.getString("defInterState",null);
+            defAmpState = prefs.getString("defAmpState",null);
+            defDvrState = prefs.getString("defDvrState",null);
+            drlDelay = prefs.getString("drlDelay",null);
+            interDelay = prefs.getString("interDelay",null);
+            ampDelay = prefs.getString("ampDelay",null);
+            dvrDelay = prefs.getString("dvrDelay",null);
+            daytimeStart = prefs.getString("daytimeStart",null);
+            daytimeEnd = prefs.getString("daytimeEnd",null);
 
         } else {
             SharedPreferences.Editor editor = prefs.edit();

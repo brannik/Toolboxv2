ae92593ab8199340c009ebb027f16d6ce5237555
brannik <georgi.vtu.mi@gmail.com>
2020-11-01 01:03:36 +0200

    control over WIFI

--- app/src/main/java/com/brannik/toolboxv2/functions.java
+++ app/src/main/java/com/brannik/toolboxv2/functions.java
@@ -1,5 +1,46 @@
 package com.brannik.toolboxv2;
 
-public class functions {
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
 
+public class functions {
+    Context applicationContext = MainActivity.getContextOfApplication();
+    SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(applicationContext);
+    String drlState,interState,ampState,dvrState,defDrlState,defInterState,defAmpState,defDvrState,drlDelay,interDelay,ampDelay,dvrDelay,daytimeStart,daytimeEnd;
+    
+    public void getSettings(){
+        // read shared prefs
+        if (prefs.contains("drlState") & prefs.contains("daytimeEnd")) {
+            drlState = prefs.getString("drlState",null);
+
+        } else {
+            SharedPreferences.Editor editor = prefs.edit();
+            editor.putString("drlState", "OFF");
+            editor.putString("interState", "OFF");
+            editor.putString("ampState", "OFF");
+            editor.putString("dvrState", "OFF");
+
+            editor.putString("defDrlState", "OFF");
+            editor.putString("defInterState", "OFF");
+            editor.putString("defAmpState", "OFF");
+            editor.putString("defDvrState", "OFF");
+
+            editor.putString("drlDelay", "10");
+            editor.putString("interDelay", "10");
+            editor.putString("ampDelay", "10");
+            editor.putString("dvrDelay", "10");
+
+            editor.putString("daytimeStart", "6");
+            editor.putString("daytimeEnd", "18");
+            editor.apply();
+            editor.commit();
+        }
+    }
+    public void setSettings(){
+        // write shared prefs
+    }
+    public void changeValue(){
+        // switch states
+    }
 }

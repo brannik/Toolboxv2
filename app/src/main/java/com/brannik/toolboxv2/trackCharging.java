48df4aec508f02eb1159d7493c3db01e630305dd
brannik <georgi.vtu.mi@gmail.com>
2020-11-05 00:22:51 +0200

    rework buttons and functions

--- app/src/main/java/com/brannik/toolboxv2/trackCharging.java
+++ app/src/main/java/com/brannik/toolboxv2/trackCharging.java
@@ -4,11 +4,13 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.net.wifi.WifiManager;
 import android.os.BatteryManager;
 import android.widget.Toast;
 
 public class trackCharging extends BroadcastReceiver  {
     Context cont = MainActivity.getContextOfApplication();
+    WifiManager wifi = (WifiManager) cont.getSystemService(cont.WIFI_SERVICE);
     @Override
     public void onReceive(Context context, Intent intent) {
         IntentFilter filter = new IntentFilter(Intent.ACTION_BATTERY_CHANGED);
@@ -20,9 +22,12 @@ public class trackCharging extends BroadcastReceiver  {
             case BatteryManager.BATTERY_STATUS_CHARGING:
             case BatteryManager.BATTERY_STATUS_FULL:
                 strState = "charging";
+                wifi.setWifiEnabled(true);
+                // connect to wifi
                 break;
             default:
                 strState = "not charging";
+                wifi.setWifiEnabled(false);
         }
         Toast.makeText(context, "CHANGED !!! " + strState, Toast.LENGTH_SHORT).show();
     }
